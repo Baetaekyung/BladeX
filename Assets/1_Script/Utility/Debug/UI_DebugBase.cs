43ffcd4cc9a1bc656648f8ae3e821281d89a88c2
zhdltl601 <zhdltl601@gmail.com>
2024-12-03 00:09:19 +0900

    Debug Base

--- Assets/1_Script/Utility/Debug/UI_DebugBase.cs
+++ Assets/1_Script/Utility/Debug/UI_DebugBase.cs
@@ -2,18 +2,28 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-namespace BossRushGame
+public abstract class UI_DebugBase<T> : MonoSingleton<T> where T : UI_DebugBase<T>
 {
-    public abstract class UI_DebugBase<T> : MonoSingleton<T> where T : UI_DebugBase<T>
+    private const bool DISABLE_ALL_UI = false;
+    [SerializeField] private bool active = true;
+    public bool ShowDebugUI
     {
-        private const bool disableALLUI = false;
-        [SerializeField] private bool active = true;
-        private bool IsActivated => active && !disableALLUI;
-        [SerializeField] private List<TextMeshProUGUI> list;
-        public IList<TextMeshProUGUI> GetList => list;
-        protected virtual void Start()
+        get => active && !DISABLE_ALL_UI;
+        set
         {
-            gameObject.SetActive(IsActivated);
+            active = value;
+            OnChange();
         }
     }
+    [SerializeField] private List<TextMeshProUGUI> list;
+    public IList<TextMeshProUGUI> GetList => list;
+    protected virtual void Start()
+    {
+        OnChange();
+    }
+    private void OnChange()
+    {
+        gameObject.SetActive(ShowDebugUI);
+    }
+
 }
