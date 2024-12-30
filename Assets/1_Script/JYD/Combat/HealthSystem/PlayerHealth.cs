0e8708f204d88ba6611cc87a0caa8dc1832d07ac
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2024-12-30 14:13:07 +0900

    Fix PlayerHealth

--- Assets/1_Script/JYD/Combat/HealthSystem/PlayerHealth.cs
+++ Assets/1_Script/JYD/Combat/HealthSystem/PlayerHealth.cs
@@ -1,44 +1,42 @@
 using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Events;
 
 namespace Swift_Blade
 {
     public class PlayerHealth : MonoBehaviour,IDamageble
     {
-        public float maxHealth;
-        public float currentHealth;
+        [SerializeField] private StatComponent _statCompo;
+        [SerializeField] private StatSO _healthStat;
 
-        public event Action OnDeadEvent;
-        public event Action OnHitEvent;
-        
-        private void Start()
-        {
-            currentHealth = maxHealth;
-            
-        }
-        
-        private void OnDestroy()
-        {
-            
-        }
+        private float _maxHealth;
+        private float _currentHealth;
 
+        public UnityEvent OnDeadEvent;
+        public UnityEvent OnHitEvent;
+                
         public void TakeDamage(ActionData actionData)
         {
             float damageAmount = actionData.damageAmount;
-            currentHealth -= damageAmount;
+            _currentHealth -= damageAmount;
             
             OnHitEvent?.Invoke();
-            
-            if (currentHealth <= 0)
-                Dead();
-        }
-        
 
-        public void TakeHeal(float amount)
+            if (_currentHealth <= 0)
+            {
+                Dead();
+            }
+        }
+
+        public void TakeHeal()
         {
-            
+            //이거 매개변수 없으면 안되는거 아닌가?
+        }
+
+        public void TakeHeal(float healAmount) //힐 받으면 현재 체력에 HealAmount 더한 값으로 변경
+        {
+            _currentHealth = Mathf.Clamp(_currentHealth + healAmount, 0, _maxHealth);
         }
 
         public void Dead()
@@ -47,5 +45,12 @@ namespace Swift_Blade
             //Debug.Log("플레이어 죽었슴");
         }
         
+        /// <param name="value">추가할 체력 값</param>
+        public void AddBaseHealth(float value) //기본 값 변경이라 키 값이 필요 없음.
+        {
+            _statCompo.SetBaseValue(_healthStat, _maxHealth + value);
+            _maxHealth = _healthStat.Value;
+            _currentHealth = Mathf.Clamp(_currentHealth + value, 0, _healthStat.Value);
+        }
     }
 }
