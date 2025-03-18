e62e2128981e49ab76605395459df728fcdc3aef
zhdltl601 <zhdltl601@gmail.com>
2025-03-18 23:45:04 +0900

    Imported Audio

--- Assets/1_Script/Core/AudioUtility/Audio/SO/AudioSOCollection.cs
+++ Assets/1_Script/Core/AudioUtility/Audio/SO/AudioSOCollection.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace Swift_Blade.Audio
 {
     [CreateAssetMenu(fileName = "AudioCollection", menuName = "Scriptable Objects/Audio/AudioCollection")]
-    public class AudioCollection : ScriptableObject
+    public class AudioSOCollection : ScriptableObject
     {
         [field: SerializeField] public AudioSO[] AudioList { get; private set; }
         public AudioSO GetRandomAudio => AudioList[Random.Range(0, AudioList.Length)];
