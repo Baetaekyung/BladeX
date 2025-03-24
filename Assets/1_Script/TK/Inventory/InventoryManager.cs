b04e56f367bb03bcc65c66cf9b894c920a88f479
Phaominho <pmhminho0516@gmail.com>
2025-03-24 23:43:10 +0900

    TutorialMapUpdate

--- Assets/1_Script/TK/Inventory/InventoryManager.cs
+++ Assets/1_Script/TK/Inventory/InventoryManager.cs
@@ -292,6 +292,7 @@ namespace Swift_Blade
 
         public int GetItemCount(ItemDataSO itemData)
         {
+            Debug.Log(itemData);
             if (_itemDatas.ContainsKey(itemData))
             {
                 return _itemDatas[itemData];
