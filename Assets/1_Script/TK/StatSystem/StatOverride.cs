c252a3fce036697700c137d3d82a0fa99873cf6e
Baetaekyung <tkwayi2129@gmail.com>
2025-03-26 20:14:12 +0900

    Color to stat

--- Assets/1_Script/TK/StatSystem/StatOverride.cs
+++ Assets/1_Script/TK/StatSystem/StatOverride.cs
@@ -6,24 +6,24 @@ namespace Swift_Blade
     [Serializable]
     public class StatOverride
     {
-        //사용 X
-        [SerializeField] private StatSO _stat;
-        [SerializeField] private bool _isUseOverride;
-        [SerializeField] private float _overrideValue;
+        ////사용 X
+        //[SerializeField] private StatSO _stat;
+        //[SerializeField] private bool _isUseOverride;
+        //[SerializeField] private float _overrideValue;
 
-        public StatOverride(StatSO stat)
-        {
-            _stat = stat;
-        }
+        //public StatOverride(StatSO stat)
+        //{
+        //    _stat = stat;
+        //}
 
-        public StatSO CreateStat()
-        {
-            StatSO newStat = _stat.Clone() as StatSO;
+        //public StatSO CreateStat()
+        //{
+        //    StatSO newStat = _stat.Clone() as StatSO;
             
-            if(_isUseOverride)
-                newStat.BaseValue = _overrideValue;
+        //    if(_isUseOverride)
+        //        newStat.ColorValue = _overrideValue;
 
-            return newStat;
-        }
+        //    return newStat;
+        //}
     }
 }
