4349642ddb04a0740f17721db52d053d6c8d9dcb
mki821 <alexalex0821@naver.com>
2025-04-07 10:53:50 -0400

    Find Animation Bug

--- Assets/1_Script/Minki/Enemy/Sorcerer/SorcererBoss.cs
+++ Assets/1_Script/Minki/Enemy/Sorcerer/SorcererBoss.cs
@@ -1,9 +1,13 @@
+using Swift_Blade.Enemy.Boss.Sword;
 using UnityEngine;
 
 namespace Swift_Blade.Enemy.Boss
 {
     public class SorcererBoss : BaseEnemy
     {
-        
+        protected override void Update()
+        {
+            
+        }
     }
 }
