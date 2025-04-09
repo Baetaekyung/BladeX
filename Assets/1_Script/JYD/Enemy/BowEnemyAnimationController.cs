997fd48b757ef05001157261ac9c2988ebd18ab1
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2025-04-09 15:31:48 +0900

    Add BowEnemy Audio and Fix Arrow Push Collision

--- Assets/1_Script/JYD/Enemy/BowEnemyAnimationController.cs
+++ Assets/1_Script/JYD/Enemy/BowEnemyAnimationController.cs
@@ -1,16 +1,26 @@
+using Swift_Blade.Audio;
 using UnityEngine;
-using Swift_Blade.Enemy.Bow;
+using System;
 
 namespace Swift_Blade.Enemy.Bow
 {
     public class BowEnemyAnimationController : BaseEnemyAnimationController
     {
         [SerializeField] private Bowstring bowstring;
-
+        
+        private event Action<AudioSO> OnAudioPlayEvent;
+        
         protected override void Start()
         {
             base.Start();
             StopDrawBowstring();
+
+            OnAudioPlayEvent += OnAudioPlayTrigger;
+        }
+
+        private void OnDestroy()
+        {
+            OnAudioPlayEvent -= OnAudioPlayTrigger;
         }
 
         public void StartDrawBowstring()
@@ -22,5 +32,14 @@ namespace Swift_Blade.Enemy.Bow
         {
             bowstring.canDraw = false;
         }
+        
+        private void OnAudioPlay(AudioSO audio) => OnAudioPlayEvent?.Invoke(audio);
+        private void OnAudioPlayTrigger(AudioSO audioSO)
+        {
+            AudioManager.PlayWithInit(audioSO, true);
+        }
+        
+        private void OnAudioPlayCollection(AudioCollectionSO audioCollectionSo) => OnAudioPlay(audioCollectionSo.GetRandomAudio);
+        
     }
 }
