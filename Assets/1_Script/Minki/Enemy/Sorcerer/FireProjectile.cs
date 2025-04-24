219610010571a41c49aa73bc228a58b04026927c
mki821 <alexalex0821@naver.com>
2025-04-24 11:03:46 +0900

    Sorcerer Attack5

---
+++ Assets/1_Script/Minki/Enemy/Sorcerer/FireProjectile.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+
+namespace Swift_Blade.Pool
+{
+    public class FireProjectile : MonoBehaviour, IPoolable
+    {
+        [SerializeField] private LayerMask _whatIsTarget;
+        
+        [SerializeField] private float _originSpeed;
+        [SerializeField] private float _pushTime;
+
+        private float _speed;
+        private float _startAngle;
+        private float _pushTimer;
+
+        public void OnPop()
+        {
+            _speed = _originSpeed;
+            _pushTimer = 0f;
+        }
+
+        public void SetAngle(float angle)
+        {
+            transform.eulerAngles = new Vector3(0f, angle, 0f);
+            _startAngle = angle;
+        }
+        
+        private void Update()
+        {
+            float percent = Mathf.Clamp(_pushTimer / _pushTime, 0f, 1f);
+            float currentAngle = Mathf.Lerp(0f, 360f, percent);
+            transform.eulerAngles = new Vector3(0f, _startAngle + currentAngle, 0f);
+
+            _speed += 10f * Time.deltaTime;
+            transform.Translate(Vector3.forward * (_speed * Time.deltaTime));
+
+            _pushTimer += Time.deltaTime;
+            if (_pushTimer > _pushTime)
+            {
+                MonoGenericPool<FireProjectile>.Push(this);
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if ((_whatIsTarget & (1 << other.gameObject.layer)) != 0)
+            {
+                if (other.gameObject.TryGetComponent(out IHealth health))
+                {
+                    health.TakeDamage(new ActionData() { damageAmount = 1, stun = true });
+                }
+            }
+        }
+    }
+}
