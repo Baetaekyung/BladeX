2d0e17f9392dc2c4011c3b3803645900a139fe01
Phaominho <pmhminho0516@gmail.com>
2025-04-30 19:47:10 +0900

    visCode

--- Assets/1_Script/PMH/MinhosLab/PlayerVisualController.cs
+++ Assets/1_Script/PMH/MinhosLab/PlayerVisualController.cs
@@ -39,7 +39,9 @@ namespace Swift_Blade
                 {
                     string offsetR = $"Chr_LegRight_Male_{partsName}";
                     string offsetL = $"Chr_LegLeft_Male_{partsName}";
+
                     //신발은 파츠네임에 숫자만기입
+                    Debug.Log($"{partsName}");
 
                     go = RightShoesVisuals.Find(offsetR).gameObject;
                     go.SetActive(true);
@@ -74,6 +76,7 @@ namespace Swift_Blade
                     string offsetR = $"Chr_LegRight_Male_{partsName}";
                     string offsetL = $"Chr_LegLeft_Male_{partsName}";
                     //신발은 파츠네임에 숫자만기입
+                    Debug.Log($"{offsetR} and {offsetL}");
 
                     go = RightShoesVisuals.Find(offsetR).gameObject;
                     go.SetActive(false);
@@ -134,15 +137,14 @@ namespace Swift_Blade
         private GameObject GetFindObj(Transform visParent, string partsName)
         {
             Transform trm = visParent.Find(partsName);
-            if (trm != null)
+            if (trm is not null)
             {
-                Debug.Log($"{visParent} 에 있습니다");
+                Debug.Log($"target on {visParent}");
                 SetOffVisuals(visParent);
 
                 return trm.gameObject;
             }
 
-            Debug.Log("겟파인트오브젝트인데 널이뜨네요");
             return null;
         }
         private void SetOffVisuals(Transform t)
