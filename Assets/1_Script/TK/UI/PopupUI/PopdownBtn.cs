1b06ff1ad699a4d658b394b6cc322fb75afb2e09
Baetaekyung <tkwayi2129@gmail.com>
2025-05-15 09:48:35 +0900

    Dotween error fix

--- Assets/1_Script/TK/UI/PopupUI/PopdownBtn.cs
+++ Assets/1_Script/TK/UI/PopupUI/PopdownBtn.cs
@@ -5,9 +5,17 @@ namespace Swift_Blade
 {
     public class PopdownBtn : BaseButton
     {
+        [SerializeField] private bool useCallback = false;
+        [SerializeField] private PopupType callbackPopupType;
+
         protected override void ClickEvent()
         {
             PopupManager.Instance.PopDown();
+
+            if(useCallback)
+            {
+                PopupManager.Instance.PopUp(callbackPopupType);
+            }
         }
     }
 }
